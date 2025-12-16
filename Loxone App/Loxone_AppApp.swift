09de2201d86f2810379ccc02881ee11ab54a160b
pianoman7304 <oli@wyrs.ch>
2025-12-16 20:44:56 +0100

    Initial Commit

---
+++ Loxone App/Loxone_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loxone_AppApp.swift
+//  Loxone App
+//
+//  Created by Oliver Wyrsch on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Loxone_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
